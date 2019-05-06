2eff41a36976276a704efb0c9e3d9d4bdc9f164c
Bartosz Solarski <bartosz.solarski@hotmail.com>
2019-05-06 21:38:31 +0200

    summary messages fixed

--- src/builder.js
+++ src/builder.js
@@ -220,6 +220,7 @@ const writeSummary = (type, dir, replaceDictionary) => {
     console.log('');
     console.log('\tor, if you want run this app in docker container:');
     console.log('');
+    console.log(`\t\tcd ${relativeDir}`);
     if (type === 'webapp') {
       console.log('\t\tnpm install');
     }
